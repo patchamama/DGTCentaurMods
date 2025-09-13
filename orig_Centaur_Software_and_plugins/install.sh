455771fbce0729f3bc18ca634a112dbad2198d60
Armando Urquiola <patchamama.go@gmail.com>
2025-09-13 17:40:56 +0200

    update done

--- orig_Centaur_Software_and_plugins/install.sh
+++ orig_Centaur_Software_and_plugins/install.sh
@@ -1,3 +1,10 @@
+sudo apt-get update
+
+sudo apt install ntp -y
+
 wget https://github.com/EdNekebno/DGTCentaurMods/releases/download/v1.3.3/dgtcentaurmods_1.3.3_armhf.deb
 
 sudo apt install -y ./dgtcentaurmods_1.3.3_armhf.deb
+
+wget https://github.com/patchamama/DGTCentaurMods/blob/master/orig_Centaur_Software_and_plugins/S03centaur
+wget https://github.com/patchamama/DGTCentaurMods/blob/master/orig_Centaur_Software_and_plugins/wifi-setup.txt
