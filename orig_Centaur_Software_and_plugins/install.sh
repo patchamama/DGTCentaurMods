5c93a67dd91fb92a30dab504f77a27b1ea262f78
Armando Urquiola <patchamama.go@gmail.com>
2025-09-14 14:10:41 +0200

    Added Alistair_DGTCentaurMods .deb installer

--- orig_Centaur_Software_and_plugins/install.sh
+++ orig_Centaur_Software_and_plugins/install.sh
@@ -6,5 +6,12 @@ wget https://github.com/EdNekebno/DGTCentaurMods/releases/download/v1.3.3/dgtcen
 
 sudo apt install -y ./dgtcentaurmods_1.3.3_armhf.deb
 
-wget https://github.com/patchamama/DGTCentaurMods/blob/master/orig_Centaur_Software_and_plugins/S03centaur
-wget https://github.com/patchamama/DGTCentaurMods/blob/master/orig_Centaur_Software_and_plugins/wifi-setup.txt
+wget https://raw.githubusercontent.com/patchamama/DGTCentaurMods/refs/heads/master/orig_Centaur_Software_and_plugins/S03centaur
+wget https://raw.githubusercontent.com/patchamama/DGTCentaurMods/refs/heads/master/orig_Centaur_Software_and_plugins/wifi-setup.txt
+
+wget https://raw.githubusercontent.com/patchamama/DGTCentaurMods/refs/heads/master/orig_Centaur_Software_and_plugins/centaur_ald_with_ring.zip
+unzip centaur_ald_with_ring.zip
+chmod +x centaur/centaur
+cp S03centaur /etc/init.d 
+chmod +x /etc/init.d/S03centaur
+
